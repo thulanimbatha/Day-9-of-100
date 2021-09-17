797fabfb83ecc6aaecd1b3f09ecc9d26fff11043
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-17 19:07:54 +0200

    added grading program exercise

---
+++ grading_program.py
@@ -0,0 +1,24 @@
+# simple grading program using dictionaries
+
+student_scores = {
+    "Harry" : 81,
+    "Ron" : 78,
+    "Hermione" : 99,
+    "Draco" : 74,
+    "Neville" : 62,
+}
+
+student_grades = {} # new dictionary
+
+# add student to new dict according to scores
+for key in student_scores:
+    if student_scores[key] > 90 and student_scores[key] <= 100:
+        student_grades[key] = "Outstanding"
+    elif student_scores[key] > 80 and student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_scores[key] > 70 and student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+# print out list
+print(student_grades)
